929ed4d699ee57a0b83a400b72f3cbbddd17e7b5
Son Roy Almerol <saalmerol@pm.me>
2019-07-20 22:24:19 +0800

    working humidity and temp

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-const DHT = require('node-dht-sensor')
-const ads1x15 = require('node-ads1x15') // install this with npm i raspi raspi-i2c --save
+const DHT = require('node-dht-sensor').promises
+var ads1x15 = require('node-ads1x15') // install this with npm i raspi raspi-i2c --save
 var ADS = new ads1x15(1)
 
-const getHT = () => {
-  return Promise((resolve, reject) => {
-    DHT.read(22, 4, function(err, temperature, humidity) {
-      if (!err) {
-        resolve([temperature, humidity])
-      }
-      reject(err)
-    })
-  })
-}
-
 const getVraw = () => {
-  return Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     ADS.readADCSingleEnded(0, '4096', '250', function(err, data) {   
       if (err) {  
         //logging / troubleshooting code goes here...  
         reject(err)
       }
       // if you made it here, then the data object contains your reading!  
+      //console.log(data)
       resolve(data)
       // any other data processing code goes here...  
     })
@@ -28,7 +18,7 @@ const getVraw = () => {
 }
 
 const getIraw = () => {
-  return Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     ADS.readADCSingleEnded(1, '6144', '250', function(err, data) {   
       if (err) {  
         //logging / troubleshooting code goes here...  
@@ -50,8 +40,8 @@ const port = 3000
 const uniqid = require('uniqid')
 const moment = require('moment')
 
-const db = new PouchDB('thesis-data')
-var remoteDB = 'http://admin:admin123@45.76.184.218:5984/thesis-data'
+const db = new PouchDB('thesis-data-demo')
+var remoteDB = 'http://admin:admin123@thesis.mikumaprojects.com/thesis-data-demo'
 const Poller = require('./poller')
 
 // Set 3s timeout between polls
@@ -59,45 +49,41 @@ const Poller = require('./poller')
 let poller = new Poller(3000)
 
 // Wait till the timeout sent our event to the EventEmitter
-poller.onPoll(() => {
+poller.onPoll(async () => {
   var Vraw, Iraw, Vout, Iout, humidity, temperature
-  if (!ADS.busy) {
-    Promise.all([getVraw(), getIraw(), getHT()]).then((res) => {
-      Vraw = res[0]
-      Iraw = res[1]
-      Vout = Vraw * 0.632034632 / 1000
-      Iout = (Iraw - 13775) * 0.000105949 / 0.1
-
-      temperature = res[2][0]
-      humidity = res[2][1]
-      
-
-      db.put({
-        _id: uniqid(),
-        timestamp: new Date(),
-        temperature,
-        humidity,
-        voltage: Vout,
-        current: Iout
-      }).catch((err) => {
-        console.log(err)
-      })
-
-      db.sync(remoteDB, {
-        live: true,
-        retry: true
-      }).on('change', function (change) {
-        console.log('data change', change)
-      }).on('error', function (err) {
-        console.log('sync error', err)
-      })
-
-      poller.poll() // Go for the next poll
-    })
-  } else {
-    console.log("ADS1115 is busy!")
-    poller.poll()
+  try {  
+    Vraw = await getVraw()
+    Iraw = await getIraw()
+    Vout = Vraw * 0.632034632 / 1000
+    Iout = (Iraw - 13775) * 0.000105949 / 0.1
+  } catch (err) {
+    console.log(err)
+    Vout = 0
+    Iout = 0
   }
+  var { temperature, humidity } = await DHT.read(22, 4)
+
+  db.put({
+    _id: uniqid(),
+    timestamp: new Date(),
+    temperature: temperature.toFixed(1),
+    humidity: humidity.toFixed(1),
+    voltage: Vout,    
+    current: Iout
+  }).catch((err) => {
+    console.log(err)
+  })
+
+  db.sync(remoteDB, {
+    live: true,
+    retry: true
+  }).on('change', function (change) {
+    console.log('data change', change)
+  }).on('error', function (err) {
+    console.log('sync error', err)
+  })
+
+  poller.poll() // Go for the next poll
 })
 
 // Initial start
